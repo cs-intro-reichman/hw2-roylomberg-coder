ff921a7a3d17ea6c16b2c44fbc2e61b744993001
Roy Lomberg <roylomberg@Roys-MacBook-Air.local>
2025-11-13 13:57:46 +0200

    hw2

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,20 @@
-// Computes an approximation of PI.
 public class CalcPi {
-	public static void main(String [] args) { 
-	    // Replace this comment with your code
-	}
+    public static void main(String[] args){
+        int n = Integer.parseInt(args[0]);
+        double sum = 0.0;
+        double z = 1.0;
+
+        for (int i = 0; i < n; i++){
+            if (i % 2 == 0){
+                sum = sum + (1.0 / z);
+            } else {
+                sum = sum - (1.0 / z);
+            }
+            z = z + 2.0;
+        }
+
+        sum = 4 * sum;
+        System.out.println("pi according to Java: " + Math.PI);
+        System.out.println("pi, approximated:     " + sum);
+    }
 }
--- Cheers.java
+++ Cheers.java
@@ -1,13 +1,25 @@
-//feedback
-
-/*
-Great work boaz!
-*/
-
-
-// Prints a crowd cheering output.
 public class Cheers {
-        public static void main(String[] args) {
-	    // Replace this comment with your code
+    public static void main(String[] args) {
+
+        String word = args[0].toUpperCase();
+        int n = Integer.parseInt(args[1]);
+        String an = "AEFHILMNORSX";
+
+        int i = 0;
+        while (i < word.length()) {
+            char c = word.charAt(i);
+            if (an.indexOf(c) >= 0) {
+                System.out.println("Give me an " + c + ": " + c + "!");
+            } else {
+                System.out.println("Give me a " + c + ": " + c + "!");
+            }
+            i++;
         }
+
+        System.out.println("What does that spell?");
+        for (int k = 0; k < n; k++) {
+            System.out.println(word + "!!!");
+        }
+        
+    }
 }
